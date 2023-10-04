a5c6f7af5dd3ac8d330098e6c54ba0ca0497f295
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2023-10-04 02:11:37 -0600

    Adding an Android Ripple Effect and iOS Alternative

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -28,7 +28,6 @@ export default function App() {
           data={courseGoals}
           alwaysBounceVertical={false}
           renderItem={(itemData) => {
-            console.log(itemData);
             return (
               <GoalItem
                 text={itemData.item.text}
