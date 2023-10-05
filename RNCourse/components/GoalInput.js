af9dccf433ccae524fdfd0047154f410bea76eb9
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2023-10-05 16:40:17 -0600

    styling the modal overlay

--- RNCourse/components/GoalInput.js
+++ RNCourse/components/GoalInput.js
@@ -24,10 +24,17 @@ const GoalInput = (props) => {
           onChangeText={goalInputHandler}
           value={enteredGoalText}
         />
-        <Button
-          title='Add Goal'
-          onPress={addGoalHandler}
-        />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button
+              title='Add Goal'
+              onPress={addGoalHandler}
+            />
+          </View>
+          <View style={styles.button}>
+            <Button title='Cancel' />
+          </View>
+        </View>
       </View>
     </Modal>
   );
@@ -38,18 +45,25 @@ export default GoalInput;
 const styles = StyleSheet.create({
   inputContainer: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
     alignItems: 'center',
     marginBottom: 24,
+    padding: 16,
     borderBottomWidth: 1,
     borderBottomColor: '#ccc',
   },
   textInput: {
     borderWidth: 1,
     borderColor: '#ccc',
-    width: '70%',
-    marginRight: 8,
+    width: '100%',
     padding: 8,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginTop: 16,
+  },
+  button: {
+    width: '30%',
+    marginHorizontal: 8,
   },
 });
