bb222ae2ff434ea5376c07d8a87a60e196006641
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2023-10-12 14:43:31 -0600

    adding a background image

--- deep-dive-starting-project/App.js
+++ deep-dive-starting-project/App.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 import StartGameScreen from './screens/StartGameScreen';
@@ -8,7 +8,13 @@ export default function App() {
     <LinearGradient
       colors={['#4e0329', '#ddb52f']}
       style={styles.rootScreen}>
-      <StartGameScreen />
+      <ImageBackground
+        source={require('./assets/images/background.png')}
+        resizeMode='cover'
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}>
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -16,5 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
   },
 });
