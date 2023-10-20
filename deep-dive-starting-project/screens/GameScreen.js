4ccb080f42b4bc0bf434be67824d0504e7ac68f1
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2023-10-20 14:17:41 -0600

    styling game round logs

--- deep-dive-starting-project/screens/GameScreen.js
+++ deep-dive-starting-project/screens/GameScreen.js
@@ -7,6 +7,7 @@ import NumberContainer from '../components/game/NumberContainer';
 import PrimaryButton from '../components/ui/PrimaryButton';
 import Card from '../components/ui/Card';
 import InstructionText from '../components/ui/InstructionText';
+import GuessLogItem from '../components/game/GuessLogItem';
 
 function generateRandomBetween(min, max, exclude) {
   const rndNum = Math.floor(Math.random() * (max - min)) + min;
@@ -62,6 +63,8 @@ function GameScreen({ userNumber, onGameOver }) {
     setCurrentGuess(newRndNumber);
     setGuessRounds((prevGuessRounds) => [newRndNumber, ...prevGuessRounds]);
   }
+
+  const guessRoundsListLength = guessRounds.length;
   return (
     <View style={styles.screen}>
       <Title>Opponent's Guess</Title>
@@ -98,7 +101,12 @@ function GameScreen({ userNumber, onGameOver }) {
         <FlatList
           keyExtractor={(item) => item}
           data={guessRounds}
-          renderItem={(itemData) => <Text>{itemData.item}</Text>}
+          renderItem={(itemData) => (
+            <GuessLogItem
+              roundNumber={guessRoundsListLength - itemData.index}
+              guess={itemData.item}
+            />
+          )}
         />
       </View>
     </View>
