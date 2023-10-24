43f9e20c0e2def4ff8600608a247605fca70a1eb
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2023-10-24 14:14:56 -0600

    setting sizes dynamically for different orientations

--- deep-dive-starting-project/screens/StartGameScreen.js
+++ deep-dive-starting-project/screens/StartGameScreen.js
@@ -1,4 +1,10 @@
-import { TextInput, View, StyleSheet, Alert, Text } from 'react-native';
+import {
+  TextInput,
+  View,
+  StyleSheet,
+  Alert,
+  useWindowDimensions,
+} from 'react-native';
 import { useState } from 'react';
 
 import PrimaryButton from '../components/ui/PrimaryButton';
@@ -9,6 +15,8 @@ import InstructionText from '../components/ui/InstructionText';
 
 function StartGameScreen({ onPickedNumber }) {
   const [enteredNumber, setEnteredNumber] = useState('');
+
+  const { width, height } = useWindowDimensions();
 
   function numberInputHandler(enteredText) {
     setEnteredNumber(enteredText);
@@ -32,8 +40,10 @@ function StartGameScreen({ onPickedNumber }) {
     onPickedNumber(chosenNumber);
   }
 
+  const marginTopDistance = height < 380 ? 30 : 100;
+
   return (
-    <View style={styles.rootContainer}>
+    <View style={[styles.rootContainer, { marginTop: marginTopDistance }]}>
       <Title>Guess My Number</Title>
       <Card>
         <InstructionText>Enter a Number</InstructionText>
@@ -61,10 +71,12 @@ function StartGameScreen({ onPickedNumber }) {
 
 export default StartGameScreen;
 
+//const deviceHeight = Dimensions.get('window').height;
+
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    marginTop: 100,
+    //marginTop: deviceHeight < 380 ? 30 : 100,
     alignItems: 'center',
   },
 
