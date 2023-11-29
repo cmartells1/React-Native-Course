8ce5b7a61fde7ea70cc9caafdd350bf863f39933
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2023-11-28 23:22:24 -0700

    Nesting Navigators

--- meals-project/App.js
+++ meals-project/App.js
@@ -2,12 +2,40 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import CategoriesScreen from './screens/CategoriesScreen';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
 import MealDetailsScreen from './screens/MealsDetailsScreen';
+import FavoritesScreen from './screens/FavoritesScreen';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: '#351401' },
+        headerTintColor: 'white',
+        sceneContainerStyle: {
+          backgroundColor: '#3f2f25',
+        },
+      }}>
+      <Drawer.Screen
+        name='Categories'
+        component={CategoriesScreen}
+        options={{
+          title: 'All Categories',
+        }}
+      />
+      <Drawer.Screen
+        name='Favorites'
+        component={FavoritesScreen}
+      />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -22,12 +50,13 @@ export default function App() {
               backgroundColor: '#3f2f25',
             },
           }}
-          initialRouteName='MealCategories'>
+          //initialRouteName='MealCategories'
+        >
           <Stack.Screen
-            name='MealCategories'
-            component={CategoriesScreen}
+            name='Drawer'
+            component={DrawerNavigator}
             options={{
-              title: 'All Categories',
+              headerShown: false,
             }}
           />
           <Stack.Screen
@@ -44,6 +73,9 @@ export default function App() {
           <Stack.Screen
             name='MealDetails'
             component={MealDetailsScreen}
+            options={{
+              title: 'About the Meal',
+            }}
           />
         </Stack.Navigator>
       </NavigationContainer>
