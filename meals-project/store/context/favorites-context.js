7c3e91da1587f9e0caa09daf621eca29495dde4e
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2023-11-29 14:40:05 -0700

    Managing App wide state with context

--- meals-project/store/context/favorites-context.js
+++ meals-project/store/context/favorites-context.js
@@ -1,4 +1,4 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 export const FavortiesContext = createContext({
   ids: [],
@@ -7,7 +7,29 @@ export const FavortiesContext = createContext({
 });
 
 function FavoritesContextProvider({ children }) {
-  return <FavortiesContext.Provider>{children}</FavortiesContext.Provider>;
+  const [favoriteMealIds, setFavoriteMealIds] = useState([]);
+
+  function addFavorite(id) {
+    setFavoriteMealIds((currentFavIds) => [...currentFavIds, id]);
+  }
+
+  function removeFavorite(id) {
+    setFavoriteMealIds((currentFavIds) => currentFavIds.filter)(
+      (mealId) => mealId !== id
+    );
+  }
+
+  const value = {
+    ids: favoriteMealIds,
+    addFavorite: addFavorite,
+    removeFavorite: removeFavorite,
+  };
+
+  return (
+    <FavortiesContext.Provider value={value}>
+      {children}
+    </FavortiesContext.Provider>
+  );
 }
 
 export default FavoritesContextProvider;
