477d9ce9cff94b72f1f036d2f3f2d8f7a1ad3bb2
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2023-11-28 23:29:18 -0700

    App finishing Touches

--- meals-project/App.js
+++ meals-project/App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Ionicons } from '@expo/vector-icons';
 
 import CategoriesScreen from './screens/CategoriesScreen';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
@@ -21,17 +22,37 @@ function DrawerNavigator() {
         sceneContainerStyle: {
           backgroundColor: '#3f2f25',
         },
+        drawerContentStyle: { backgroundColor: '#351401' },
+        drawerInactiveTintColor: 'white',
+        drawerActiveTintColor: '#351401',
+        drawerActiveBackgroundColor: '#e4baa1',
       }}>
       <Drawer.Screen
         name='Categories'
         component={CategoriesScreen}
         options={{
           title: 'All Categories',
+          drawerIcon: ({ color, size }) => (
+            <Ionicons
+              color={color}
+              size={size}
+              name='list'
+            />
+          ),
         }}
       />
       <Drawer.Screen
         name='Favorites'
         component={FavoritesScreen}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <Ionicons
+              color={color}
+              size={size}
+              name='star'
+            />
+          ),
+        }}
       />
     </Drawer.Navigator>
   );
