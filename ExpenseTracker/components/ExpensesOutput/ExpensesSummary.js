049d46048fdf04a2d9f8f28b550f058d4eda07ee
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2023-12-01 14:14:49 -0700

    Working on expense components

--- ExpenseTracker/components/ExpensesOutput/ExpensesSummary.js
+++ ExpenseTracker/components/ExpensesOutput/ExpensesSummary.js
@@ -1,10 +1,16 @@
 import { View, Text } from 'react-native';
 
-function ExpensesSummary() {
-  <View>
-    <Text>Last 7 Days</Text>
-    <Text>$177.95</Text>
-  </View>;
+function ExpensesSummary({ periodName, expenses }) {
+  const expensesSum = expenses.reduce((sum, expense) => {
+    return sum + expense.amount;
+  }, 0);
+
+  return (
+    <View>
+      <Text>{periodName}</Text>
+      <Text>${expensesSum.toFixed(2)}</Text>
+    </View>
+  );
 }
 
 export default ExpensesSummary;
