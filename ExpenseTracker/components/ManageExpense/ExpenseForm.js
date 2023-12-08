16577f5ab71001d5fab0a5db28a9d31668c2bbf6
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2023-12-08 13:47:49 -0700

    providing visual validation feedback

--- ExpenseTracker/components/ManageExpense/ExpenseForm.js
+++ ExpenseTracker/components/ManageExpense/ExpenseForm.js
@@ -6,26 +6,36 @@ import Button from '../UI/Button';
 import { getFormattedDate } from '../../util/date';
 
 function ExpenseForm({ submitButtonLabel, defaultValues, onCancel, onSubmit }) {
-  const [inputValues, setInputValues] = useState({
-    amount: defaultValues ? defaultValues.amount.toString() : '',
-    date: defaultValues ? getFormattedDate(defaultValues.date) : '',
-    description: defaultValues ? defaultValues.description : '',
+  const [inputs, setInputs] = useState({
+    amount: {
+      value: defaultValues ? defaultValues.amount.toString() : '',
+      isValid: true,
+    },
+    date: {
+      value: defaultValues ? getFormattedDate(defaultValues.date) : '',
+      //isValid: !!defaultValues,
+      isValid: true,
+    },
+    description: {
+      value: defaultValues ? defaultValues.description : '',
+      isValid: true,
+    },
   });
 
   function inputChangedHandler(inputIdentifier, enteredValue) {
-    setInputValues((curInputValues) => {
+    setInputs((curInputs) => {
       return {
-        ...curInputValues,
-        [inputIdentifier]: enteredValue,
+        ...curInputs,
+        [inputIdentifier]: { value: enteredValue, isValid: true },
       };
     });
   }
 
   function submitHandler() {
     const expenseData = {
-      amount: +inputValues.amount,
-      date: new Date(inputValues.date),
-      description: inputValues.description,
+      amount: +inputs.amount.value,
+      date: new Date(inputs.date.value),
+      description: inputs.description.value,
     };
 
     const amountIsValid = !isNaN(expenseData.amount) && expenseData.amount > 0;
@@ -33,12 +43,27 @@ function ExpenseForm({ submitButtonLabel, defaultValues, onCancel, onSubmit }) {
     const descriptionIsValid = expenseData.description.trim().length > 0;
 
     if (!amountIsValid || !dateIsValid || !descriptionIsValid) {
-      Alert.alert('Invalid input, Please check your input values');
+      // Alert.alert('Invalid input, Please check your input values');
+      setInputs((curInputs) => {
+        return {
+          amount: { value: curInputs.amount.value, isValid: amountIsValid },
+          date: { value: curInputs.date.value, isValid: dateIsValid },
+          description: {
+            value: curInputs.description.value,
+            isValid: descriptionIsValid,
+          },
+        };
+      });
       return;
     }
 
     onSubmit(expenseData);
   }
+
+  const formIsInvalid =
+    !inputs.amount.isValid ||
+    !inputs.date.isValid ||
+    !inputs.description.isValid;
 
   return (
     <View style={styles.form}>
@@ -50,7 +75,7 @@ function ExpenseForm({ submitButtonLabel, defaultValues, onCancel, onSubmit }) {
           textInputConfig={{
             keyboardType: 'decimal-pad',
             onChangeText: inputChangedHandler.bind(this, 'amount'),
-            value: inputValues.amount,
+            value: inputs.amount.value,
           }}
         />
         <Input
@@ -60,7 +85,7 @@ function ExpenseForm({ submitButtonLabel, defaultValues, onCancel, onSubmit }) {
             placeholder: 'YYYY-MM-DD',
             maxLength: 10,
             onChangeText: inputChangedHandler.bind(this, 'date'),
-            value: inputValues.date,
+            value: inputs.date.value,
           }}
         />
       </View>
@@ -69,9 +94,12 @@ function ExpenseForm({ submitButtonLabel, defaultValues, onCancel, onSubmit }) {
         textInputConfig={{
           multiline: true,
           onChangeText: inputChangedHandler.bind(this, 'description'),
-          value: inputValues.description,
+          value: inputs.description.value,
         }}
       />
+      {formIsInvalid && (
+        <Text>Invalid input values please check your data.</Text>
+      )}
       <View style={styles.buttonContainer}>
         <Button
           mode='flat'
