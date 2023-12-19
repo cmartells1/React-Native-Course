fedb8a99a4c3ce3d209b7a2d037f96c525c94ea3
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2023-12-19 15:15:31 -0700

    fetching backend data

--- ExpenseTracker/util/http.js
+++ ExpenseTracker/util/http.js
@@ -1,8 +1,26 @@
 import axios from 'axios';
 
+const BACKEND_URL =
+  'https://react-native-course-1e924-default-rtdb.firebaseio.com';
+
 export function storeExpense(expenseData) {
-  axios.post(
-    'https://react-native-course-1e924-default-rtdb.firebaseio.com/expenses.json',
-    expenseData
-  );
+  axios.post(BACKEND_URL + '/expenses.json', expenseData);
+}
+
+export async function fetchExpenses() {
+  const response = await axios.get(BACKEND_URL + '/expenses.json');
+
+  console.log(response);
+  const expenses = [];
+  for (const key in response.data) {
+    const expenseObj = {
+      id: key,
+      amount: repsonse.data[key].amount,
+      date: new Date(response.data[key].date),
+      description: response.data[key].description,
+    };
+    expenses.push(expenseObj);
+  }
+
+  return expenses;
 }
